69777b051fa3e53e3084f5cc6956d529a8760931
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2019-04-06 00:09:20 +0900

    Make LogisticRegressionGD

--- chapter3/logistic_regression_gd.py
+++ chapter3/logistic_regression_gd.py
@@ -7,26 +7,26 @@ class LogisticRegressionGD(object):
         self.n_iter = n_iter
         self.random_state = random_state
 
-    # def fit(self, X, y):
-    #     rgen = np.random.RandomState(self.random_state)
-    #     self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
-    #     self.cost_ = []
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
 
-    #     for _ in range(self.n_iter):
-    #         net_input = self.net_input(X)
-    #         output = self.activation(net_input)
-    #         errors = y - output
-    #         self.w_[1:] += self.eta * X.T.dot(errors)
-    #         self.w_[0] += self.eta * errors.sum()
-    #         cost = (errors ** 2).sum() / 2.0
-    #         self.cost_.append(cost)
-    #     return self
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = y - output
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
+            self.cost_.append(cost)
+        return self
 
-    # def net_input(self, X):
-    #     return np.dot(X, self.w_[1:]) + self.w_[0]
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
 
-    # def activation(self, X):
-    #     return X
+    def activation(self, X):
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
 
-    # def predict(self, X):
-    #     return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
